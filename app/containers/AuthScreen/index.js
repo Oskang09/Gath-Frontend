040ad64fffe7145d6bf4aec3b9a2d317744bdcf4
devOska <developer.oskang@gmail.com>
2019-06-03 20:51:05 +0800

    Implement GoogleMaps

--- app/containers/AuthScreen/index.js
+++ app/containers/AuthScreen/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class AuthScreen extends React.PureComponent {
 
     render() {
         return (
-            <View>
-                <Text>Auth Screen</Text>
-            </View>
+            <MapView
+                style={{flex: 1}}
+                provider={PROVIDER_GOOGLE}
+                region={{
+                    latitude: 42.882004,
+                    longitude: 74.582748,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}
+                showsUserLocation={true}
+            />
         );
     }
 };
--- ios/gath/AppDelegate.m
+++ ios/gath/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDFwlnZpmRnD4tr7C_Fy4NdjoWybnjhH_M"]
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"gath"
